eafb73e8ede208d0c015f5c1a5ae20779e4de6c1
madhurasekar9 <sekam810@newschool.edu>
2023-03-29 11:48:49 -0400

    API Check

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-
 const apiKey = "AIzaSyAbb4g0n7gb1bxtGL3cf1Wm9lvHsgZmGRM";
 const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${apiKey}&query=galleries+in+nyc`;
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 const galleryList = document.getElementById("gallery-list");
 const boroughFilter = document.getElementById("borough-filter");
 
-fetch(apiUrl)
+fetch(proxyurl+apiUrl)
   .then(response => response.json())
   .then(data => {
     const galleries = data.results.filter(result => {
