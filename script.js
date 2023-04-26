c65203b79d3599d87062ab544fdf52c85e33ff5e
madhurasekar9 <sekam810@newschool.edu>
2023-04-26 04:08:13 -0400

    footer

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const searchBar = document.getElementById('search-bar');
-const cards = document.querySelectorAll('.card');
+// const searchBar = document.getElementById('search-bar');
+// const cards = document.querySelectorAll('.card');
 
-searchBar.addEventListener('keyup', (e) => {
-  const searchText = e.target.value.toLowerCase();
-  cards.forEach(card => {
-    const cardText = card.textContent.toLowerCase();
-    if (cardText.includes(searchText)) {
-      card.style.display = 'block';
-      if (card.id !== 'card-1') {
-        card.style.marginTop = '30px';
-      }
-    } else {
-      card.style.display = 'none';
-    }
-  });
-});
+// searchBar.addEventListener('keyup', (e) => {
+//   const searchText = e.target.value.toLowerCase();
+//   cards.forEach(card => {
+//     const cardText = card.textContent.toLowerCase();
+//     if (cardText.includes(searchText)) {
+//       card.style.display = 'block';
+//       if (card.id !== 'card-1') {
+//         card.style.marginTop = '30px';
+//       }
+//     } else {
+//       card.style.display = 'none';
+//     }
+//   });
+// });
 
 // const searchInput = document.getElementById('search-input');
 // const cards = document.querySelectorAll('.card');
@@ -32,6 +32,8 @@ searchBar.addEventListener('keyup', (e) => {
 //     }
 //   });
 // });
+
+
 
 
 // const colors = ['#f44336', '#2196f3', '#4caf50', '#ffc107'];
